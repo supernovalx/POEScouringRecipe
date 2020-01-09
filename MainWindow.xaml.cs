718879a6ffbe56c40cb2a9fc35505d2279f83cf7
supernovalx <hoanganhgo9@gmail.com>
2020-01-09 12:40:02 +0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,239 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POEScouringRecipe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isDragable = false;
+        private bool isShowGridLine = false;
+        private List<Tab> tabs = new List<Tab>();
+        private Stash curStash = new Stash();
+        private Random rd = new Random();
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 12; i++)
+            {
+                gridInventory.RowDefinitions.Add(new RowDefinition());
+                gridInventory.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            tbSSID.Text = Properties.Settings.Default.ssid;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("a");
+
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && isDragable)
+                this.DragMove();
+            //Point position = gridInventory.PointToScreen(new Point(0d, 0d));
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            //Window_Closing(null, null);
+            Application.Current.Shutdown();
+
+        }
+
+        private void btnDragable_Click(object sender, RoutedEventArgs e)
+        {
+            isDragable = !isDragable;
+            btnDragable.FontWeight = isDragable ? FontWeights.Bold : FontWeights.Normal;
+        }
+
+        private void tbSSID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Properties.Settings.Default.ssid = tbSSID.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        private void btnFetch_Click(object sender, RoutedEventArgs e)
+        {
+            Stash s = GetStash(0);
+            tabs = s.tabs;
+
+            panelStashTab.Children.Clear();
+            foreach(Tab t in tabs)
+            {
+                Button b = new Button();
+                b.Click += StashTab_Click;
+                b.Content = t.n;
+                b.DataContext = t.i;
+                b.Background = new SolidColorBrush(Color.FromRgb((byte)t.colour.r, (byte)t.colour.g, (byte)t.colour.b));
+                b.Style = (Style)gridControl.Resources["style"];
+                panelStashTab.Children.Add(b);
+            }
+            tbStatus.Text = $"Fetched {s.tabs.Count} stashs!";
+
+        }
+
+        private void StashTab_Click(object sender, RoutedEventArgs e)
+        {
+            curStash = GetStash(int.Parse((e.Source as Button).DataContext.ToString()));
+
+            foreach(Control c in panelStashTab.Children)
+            {
+                c.FontWeight = FontWeights.Normal;
+            }
+            (e.Source as Button).FontWeight = FontWeights.Bold;
+            tbStatus.Text = $"Loaded {curStash.items.Count} items!";
+        }
+
+        private Stash GetStash(int index)
+        {
+            HttpWebRequest rq = (HttpWebRequest)WebRequest.Create("https://www.pathofexile.com/character-window/get-stash-items?league=metamorph&tabs=1&tabIndex=" + index + "&accountName=supernovalx6");
+            rq.CookieContainer = new CookieContainer();
+            rq.CookieContainer.Add(new Cookie("POESESSID", tbSSID.Text, "/", "pathofexile.com"));
+
+            HttpWebResponse resp = (HttpWebResponse)rq.GetResponse();
+            Stream receiveStream = resp.GetResponseStream();
+            StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+            string cont = readStream.ReadToEnd();
+
+            return JsonConvert.DeserializeObject<Stash>(cont);
+        }
+
+        private void btnGridLine_Click(object sender, RoutedEventArgs e)
+        {
+            isShowGridLine = !isShowGridLine;
+            gridInventory.ShowGridLines = isShowGridLine;
+            btnGridLine.FontWeight = isShowGridLine ? FontWeights.Bold : FontWeights.Normal;
+        }
+
+        private void Window_SourceInitialized(object sender, EventArgs e)
+        {
+            this.Top = Properties.Settings.Default.t;
+            this.Left = Properties.Settings.Default.l;
+            this.Height = Properties.Settings.Default.h;
+            this.Width = Properties.Settings.Default.w;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Properties.Settings.Default.t = this.Top;
+            Properties.Settings.Default.l = this.Left;
+            Properties.Settings.Default.h = this.Height;
+            Properties.Settings.Default.w = this.Width;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void HighLight(int x,int y)
+        {
+            Rectangle r = new Rectangle();
+            r.Stroke = new SolidColorBrush(Colors.Yellow);
+            r.StrokeThickness = 2;
+            gridInventory.Children.Add(r);
+            Grid.SetRow(r, y);
+            Grid.SetColumn(r, x);
+        }
+
+        private void ClearAllHighlight()
+        {
+            gridInventory.Children.Clear();
+        }
+
+        private void btnTransmute_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            ClearAllHighlight();
+            foreach (Item i in curStash.items)
+            {
+                if (!isMagic(i.typeLine) && i.identified)
+                {
+                    HighLight(i.x, i.y);
+                    count++;
+                }
+            }
+            tbStatus.Text = $"Transmute {count}/{curStash.items.Count} items!";
+        }
+
+        private void btnRegal_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            ClearAllHighlight();
+            foreach (Item i in curStash.items)
+            {
+                if (isRegalable(i))
+                {
+                    HighLight(i.x, i.y);
+                    count++;
+                }
+            }
+            tbStatus.Text = $"Regal {count}/{curStash.items.Count} items!";
+        }
+
+        private bool isMagic(string name)
+        {
+            return name.Split(' ').Length > 2;
+        }
+
+        private bool isRegalable(Item it)
+        {
+            bool isRing = it.typeLine.Contains("Ring");
+            bool isAmulet = it.typeLine.Contains("Amulet");
+            bool hasSuffix = it.typeLine.Contains("of");
+
+            string[] s = it.typeLine.Split(' ');
+            
+            bool hasPrefix = s[1] != (isRing ? "Ring" : "Amulet");
+
+            return hasPrefix ^ hasSuffix;
+        }
+
+        private void btnIdentify_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            ClearAllHighlight();
+            foreach (Item i in curStash.items)
+            {
+                if (!i.identified)
+                {
+                    HighLight(i.x, i.y);
+                    count++;
+                }
+            }
+            tbStatus.Text = $"Identify {count}/{curStash.items.Count} items!";
+        }
+
+        private void btnSell_Click(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            ClearAllHighlight();
+            foreach (Item i in curStash.items)
+            {
+                if (i.name!="")
+                {
+                    HighLight(i.x, i.y);
+                    count++;
+                }
+            }
+            tbStatus.Text = $"Sell {count}/{curStash.items.Count} items!";
+        }
+    }
+}
---
+++ Stash.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POEScouringRecipe
+{
+    public class Colour
+    {
+        public int r { get; set; }
+        public int g { get; set; }
+        public int b { get; set; }
+    }
+
+    public class Tab
+    {
+        public string n { get; set; }
+        public int i { get; set; }
+        public string id { get; set; }
+        public string type { get; set; }
+        public bool hidden { get; set; }
+        public bool selected { get; set; }
+        public Colour colour { get; set; }
+        public string srcL { get; set; }
+        public string srcC { get; set; }
+        public string srcR { get; set; }
+    }
+
+    public class Socket
+    {
+        public int group { get; set; }
+        public string attr { get; set; }
+        public string sColour { get; set; }
+    }
+
+    public class Property
+    {
+        public string name { get; set; }
+        public List<object> values { get; set; }
+        public int displayMode { get; set; }
+        public int type { get; set; }
+    }
+
+    public class Requirement
+    {
+        public string name { get; set; }
+        public List<List<object>> values { get; set; }
+        public int displayMode { get; set; }
+    }
+
+    public class Item
+    {
+        public bool verified { get; set; }
+        public int w { get; set; }
+        public int h { get; set; }
+        public int ilvl { get; set; }
+        public string icon { get; set; }
+        public string league { get; set; }
+        public string id { get; set; }
+        public List<Socket> sockets { get; set; }
+        public string name { get; set; }
+        public string typeLine { get; set; }
+        public bool identified { get; set; }
+        public List<Property> properties { get; set; }
+        public List<Requirement> requirements { get; set; }
+        public int frameType { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public string inventoryId { get; set; }
+        public List<object> socketedItems { get; set; }
+        public List<string> implicitMods { get; set; }
+    }
+
+    public class Stash
+    {
+        public int numTabs { get; set; }
+        public List<Tab> tabs { get; set; }
+        public List<Item> items { get; set; }
+    }
+}
